f0380f2a1c07cebdddae3383b3ad0b82eb39d8e8
SalehMKhames <122265512+SalehMKhames@users.noreply.github.com>
2024-05-01 18:35:46 +0300

    second commit. After adding the userLogin & userRegister to the abstracted user_repo in the domain file

--- lib/authentication/domain/repositiories/user_repo.dart
+++ lib/authentication/domain/repositiories/user_repo.dart
@@ -1,4 +1,12 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:doctory/authentication/domain/entities/user.dart';
+
 abstract class UserRepo
 {
-  
+  Future<Either<Failure, User>> userLogin(User userData);
+
+  Future<Either<Failure, User>> userRegister(User userData);
+
+  //Future<Either<Failure,User>> auth(String token);
 }
