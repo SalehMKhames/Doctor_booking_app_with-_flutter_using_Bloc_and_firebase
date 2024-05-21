df33401c235c968099993fb0abc2914dccade3d6
SalehMKhames <122265512+SalehMKhames@users.noreply.github.com>
2024-05-21 11:46:12 +0300

    Finishing everything in all files

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'firebase_options.dart';
 
 void main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
