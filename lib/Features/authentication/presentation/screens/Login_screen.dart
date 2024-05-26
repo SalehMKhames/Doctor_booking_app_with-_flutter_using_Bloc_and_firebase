aa6e8199c3ef35f4c77a091d25a4ea58a7cc7d01
SalehMKhames <122265512+SalehMKhames@users.noreply.github.com>
2024-05-26 12:12:40 +0300

    Adding the Dependencty injection and starting with the screens

--- lib/Features/authentication/presentation/screens/Login_screen.dart
+++ lib/Features/authentication/presentation/screens/Login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
--- lib/Features/authentication/presentation/screens/Register_screen.dart
+++ lib/Features/authentication/presentation/screens/Register_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
