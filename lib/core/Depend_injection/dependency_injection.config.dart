890cd93d8955a89859d7abc442e7c181dd2ab23d
SalehMKhames <122265512+SalehMKhames@users.noreply.github.com>
2024-06-01 18:21:56 +0300

    Changing the Depandency injection from manual GetIt to use Auto generated dependency_injection.dart by using Injectable package

---
+++ lib/core/Depend_injection/dependency_injection.config.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:http/http.dart' as _i3;
+import 'package:injectable/injectable.dart' as _i2;
+
+import 'dependency_injection.dart' as _i4;
+
+// initializes the registration of main-scope dependencies inside of GetIt
+_i1.GetIt $initialGetIt(
+  _i1.GetIt getIt, {
+  String? environment,
+  _i2.EnvironmentFilter? environmentFilter,
+}) {
+  final gh = _i2.GetItHelper(
+    getIt,
+    environment,
+    environmentFilter,
+  );
+  final injectableModule = _$InjectableModule();
+  gh.singleton<_i3.Client>(() => injectableModule.client);
+  return getIt;
+}
+
+class _$InjectableModule extends _i4.InjectableModule {}
